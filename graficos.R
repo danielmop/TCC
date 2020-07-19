8e3ab870c741d642f176b2e0483294b154b3ca79
Lesandro Ponciano <lesandropcodes@users.noreply.github.com>
2020-07-18 22:26:05 -0300

    Adicionando o script que gera o gráfico de problemas em PDF com R
    Co-Authored-By: Lesandro Ponciano <lesandrop@users.noreply.github.com>
    Co-Authored-By: Lesandro Ponciano <lesandrop@yahoo.com.br>

---
+++ graficos.R
@@ -0,0 +1,21 @@
+library(ggplot2)
+
+#Path to the main folder
+setwd("C:\\Users\\lesandrop\\Documents\\GitHub\\TCC")
+
+df <- data.frame(qproblemas=c(20,53,63,96,146,272,909,1155,3302,3465,10963,19359),
+	projeto=c('apacteca','pectometro','Kanleitos','Gestao CSF','Bartech','Private Class','Alpmys','Will List','Novolharua','Apac','gmfonseca','Projeto Incluir'))
+
+
+p <- ggplot(data=df, aes(x=projeto, y=qproblemas)) +
+geom_bar(stat="identity") +
+geom_text(stat="identity", aes(label=qproblemas),  vjust=-1) + 
+theme_bw(base_size = 12) + 
+theme(axis.text.x = element_text(angle = 75, hjust = 1)) +
+xlab("Projeto de TIS") +
+ylab("Quantidade de problemas encontrados")
+	
+
+pdf("Figuras/ProblemasPorProjeto.pdf", width = 7, height = 4)
+plot(p)
+dev.off()
