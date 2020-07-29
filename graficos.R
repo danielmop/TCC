5b5de23d1d6edfa6fd255c2b7e40712e98025c94
Lesandro Ponciano <lesandropcodes@users.noreply.github.com>
2020-07-29 17:14:30 -0300

    Ajuste da figura de problemas por projeto e inclusão de projetos faltantes
    Co-Authored-By: Lesandro Ponciano <lesandrop@yahoo.com.br>
    Co-Authored-By: Lesandro Ponciano <lesandrop@users.noreply.github.com>

--- graficos.R
+++ graficos.R
@@ -3,19 +3,19 @@ library(ggplot2)
 #Path to the main folder
 setwd("C:\\Users\\lesandrop\\Documents\\GitHub\\TCC")
 
-df <- data.frame(qproblemas=c(20,53,63,96,146,272,909,1155,3302,3465,10963,19359),
-	projeto=c('apacteca','pectometro','Kanleitos','Gestao CSF','Bartech','Private Class','Alpmys','Will List','Novolharua','Apac','gmfonseca','Projeto Incluir'))
+df <- data.frame(qproblemas=c(0,0,20,53,63,96,146,272,909,1155,3302,3465,10963,19359,654726,1176143,2186648,2232841),
+                 projeto=c('Jogo de Filosofia','Automecânica\nVailante','apacteca','pectometro','Kanleitos','Gestao CSF','Bartech','Private Class','Alpmys','Will List','Novolharua','Apac','gmfonseca','Projeto Incluir','Dcbio','Pain-o-matic','Age of Philosophy','Calf Generator'))
 
 
-p <- ggplot(data=df, aes(x=projeto, y=qproblemas)) +
+p <- ggplot(data=df, aes(x=reorder(projeto,qproblemas), y=qproblemas)) +
 geom_bar(stat="identity") +
-geom_text(stat="identity", aes(label=qproblemas),  vjust=-1) + 
 theme_bw(base_size = 12) + 
+scale_y_log10(breaks = scales::trans_breaks("log10", function(x) 10^x), labels = scales::trans_format("log10", scales::math_format(10^.x))) +
 theme(axis.text.x = element_text(angle = 75, hjust = 1)) +
 xlab("Projeto de TIS") +
  ylab("#Problemas (erros + warnings)")
 	
 
-pdf("Figuras/ProblemasPorProjeto.pdf", width = 7, height = 4)
+pdf("Figuras/ProblemasPorProjeto.pdf", width = 8, height = 6)
 plot(p)
 dev.off()
